4e6c07534d50700e8736bd1aaeccbd28a64b3970
Taylor Boren <taylor.boren@starz.com>
2017-05-27 11:08:48 -0600

    refactored main file

---
+++ constants.js
@@ -0,0 +1,13 @@
+module.exports = {
+  ANSWERS: {
+    NO: 'No',
+    YES: 'Yes'
+  },
+  MAX_TRANSACTIONS: 10,
+  STOLEN_PLATE: '1111111',
+  VEHICLES: {
+    CAR: 'Car',
+    TRUCK: 'Truck',
+    SPACESHIP: 'Spaceship'
+  },
+};
